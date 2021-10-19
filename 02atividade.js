b518986ef8a150c4078c0f1d4edc69aded76fe73
camilacentelo <camilacentelo8@gmail.com>
2021-10-18 22:13:38 -0300

    atividades do dia 18.10.21

---
+++ 02atividade.js
@@ -0,0 +1,7 @@
+var media = 4;
+if(media >= 7) {
+    console.log("aprovado");
+} else {
+console.log("reprovado");
+}
+
---
+++ 03atividade.js
@@ -0,0 +1,8 @@
+var media = 4;
+var faltas = 1;
+if(media >= 7){
+    console.log("recuperação");
+} else{
+    console.log("reprovado");
+}
+
